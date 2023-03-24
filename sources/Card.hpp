fe5dd6a46f0eb8388c40df9cff592dd7436b166a
SimchaTeich <sim.teich@gmail.com>
2023-03-24 16:01:45 +0300

    create Card class

---
+++ sources/Card.hpp
@@ -0,0 +1,23 @@
+#ifndef CARD_HPP
+#define CARD_HPP
+
+#include <string>
+using std::string
+
+class Card
+{
+private:
+    int _value;
+    string _color;
+    string _type;
+    string _specialName;
+
+public:
+    Card(int value, string color);
+
+    int getValue();
+    void printCard();
+};
+
+
+#endif // !CARD_HPP
