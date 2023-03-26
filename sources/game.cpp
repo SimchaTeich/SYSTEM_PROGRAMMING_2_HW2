934a6ab084698a54f4726ac51de8afd7da090871
SimchaTeich <sim.teich@gmail.com>
2023-03-26 20:02:38 +0300

    create game.cpp (without implement yet)

---
+++ sources/game.cpp
@@ -0,0 +1,48 @@
+#include "game.hpp"
+
+Game::Game(Player& p1, Player& p2)
+{
+
+}
+
+
+Game::~Game()
+{
+
+}
+
+
+void Game::playTurn()
+{
+
+}
+
+
+void Game::playAll()
+{
+
+}
+
+
+void Game::printLastTurn() const
+{
+
+}
+
+
+void Game::printLog() const
+{
+
+}
+
+
+void Game::printWiner() const
+{
+
+}
+
+
+void Game::printStats() const
+{
+
+}
