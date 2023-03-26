b3d752934396e711cfb9dd2e82f80674e2f53da7
SimchaTeich <sim.teich@gmail.com>
2023-03-26 19:59:21 +0300

    create card.cpp (without implement yet)

--- sources/card.cpp
+++ sources/card.cpp
@@ -2,18 +2,17 @@
 
 Card::Card(int value, const string& color)
 {
-    #TODO
+
 }
 
 
 int Card::getValue() const
 {
-    #TODO
     return 0;
 }
 
 
 void Card::printCard() const
 {
-    #TODO
+
 }
