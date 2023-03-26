ed20c3b7e6831afd0ec4e0b679c2846b39354d43
SimchaTeich <sim.teich@gmail.com>
2023-03-26 20:07:54 +0300

    create player.cpp (without implement yet)

---
+++ sources/player.cpp
@@ -0,0 +1,66 @@
+#include "player.cpp"
+
+Player::Player(const string& name)
+{
+
+}
+
+
+Player::~Player()
+{
+
+}
+
+
+void Player::insertCard(const Card& card)
+{
+
+}
+
+
+Card Player::playCard()
+{
+
+}
+
+
+void Player::winTurn(int score, const string& card)
+{
+
+}
+
+
+void Player::drawTurn(const string& card)
+{
+
+}
+
+
+string Player::name() const
+{
+
+}
+
+
+int Player::stacksize() const
+{
+
+}
+
+
+int Player::cardesTaken() const
+{
+
+}
+
+
+queue<string> Player::cardsWasDraw() const
+{
+
+}
+
+
+queue<string> Player::cartdWasWin() const
+{
+
+}
